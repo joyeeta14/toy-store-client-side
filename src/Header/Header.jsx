ff179236c2397376b6f1b97023bef3f23c6a282d
Joyeeta Sarkar <joyeetasarkar1410@gmail.com>
2023-05-19 12:36:53 +0600

    netlify reload problem fixed

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52">
                             <li><Link to='/'>Home</Link ></li>
                             <li tabIndex={0}>
                                 <Link to='/all-toys' className="justify-between">
@@ -65,7 +65,7 @@ const Header = () => {
                                 <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                             </div>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52">
                             <li>
                                 <a className="justify-between">
                                     Profile
